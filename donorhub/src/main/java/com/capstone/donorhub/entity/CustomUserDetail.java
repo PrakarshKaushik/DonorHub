d826780175a95d79f4a6036f14ace53ad3302e48
PrakarshKaushik <prakarsh.kaushik@globallogic.com>
2023-05-10 15:13:26 +0530

    Removing Conflicts

---
+++ donorhub/src/main/java/com/capstone/donorhub/entity/CustomUserDetail.java
@@ -0,0 +1,57 @@
+package com.capstone.donorhub.entity;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+
+public class CustomUserDetail implements UserDetails {
+private User user;
+	
+	
+	public CustomUserDetail(User user) {
+		this.user = user;
+	}
+
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		HashSet<SimpleGrantedAuthority> set = new HashSet<>();
+		set.add(new SimpleGrantedAuthority(this.user.getRole()));
+		return set;
+	}
+
+	public String getPassword() {
+		return this.user.getPassword();
+	}
+
+
+	public String getUsername() {
+		return this.user.getEmail();
+	}
+
+
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+
+	public boolean isAccountNonLocked() {
+		boolean flag = false;
+		if(this.user.getAccount_status().equals("unblocked"))
+			flag=true;
+		return flag;
+		
+	}
+
+
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	
+	public boolean isEnabled() {
+		return true;
+		}
+}
--- donorhub/src/main/java/com/capstone/donorhub/service/AdminServiceImpl.java
+++ donorhub/src/main/java/com/capstone/donorhub/service/AdminServiceImpl.java
@@ -12,51 +12,42 @@ import com.capstone.donorhub.respository.UserRepository;
 @Service
 public class AdminServiceImpl {
 
-    @Autowired
-    private UserRepository userRepository;
+	@Autowired
+	private UserRepository userRepository;
+
+	public List<User> getAllUsers() {
+		return userRepository.findAll();
+	}
+
+	public User saveUser(User user) {
+		return userRepository.save(user);
+	}
 
 
-   
-    public List<User> getAllUsers() {
-        return userRepository.findAll();
-    }
+	public User getSingleUser(int id) {
+		java.util.Optional<User> userOptional = userRepository.findById(id);
+		if (userOptional.isPresent()) {
+			return userOptional.get();
+		}
+		throw new RuntimeException("User not found for id: " + id);
+
+	}
 
 
-  
-    public User saveUser(User userEntity) {
+	public void deleteUser(int id) {
+		userRepository.deleteById(id);
+	}
 
-        return userRepository.save(userEntity);
-    }
+	public User updatUser(User user) {
+		return userRepository.save(user);
+	}
+ 
 
-
-  
-    public User getSingleUser(int id) {
-        java.util.Optional<User> userOptional = userRepository.findById(id);
-        if(userOptional.isPresent()) {
-            return userOptional.get();
-        }
-        throw new RuntimeException("User not found for id: "+id);
-
-    }
-
-
-  
-    public void deleteUser(int id) {
-        userRepository.deleteById(id);
-
-    }
-
-
-   
-    public User updatUser(User user) {
-        return userRepository.save(user);
-    }
-
-
-    
     public List<User> getUserByName(String name) {
 
         return userRepository.findByName(name);
     }
-}
 
+	
+
+}
